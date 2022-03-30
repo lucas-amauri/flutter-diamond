06fa51bc3e421b8a90c7f0a7f2d878406cb6dca4
lucas-amauri <lucasamauri.ls@gmail.com>
2022-03-30 12:46:51 -0400

    Fix user profile class name

--- lib/components/UserProfilePhoto.dart
+++ lib/components/UserProfilePhoto.dart
@@ -1,7 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
-class UserProfilePhoto {
+class DiamondUserProfilePhoto {
   static create({
     double radius : 50,
     String? url,
