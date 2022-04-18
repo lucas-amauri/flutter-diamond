7ea13ee1702ce2c69e68d56365987ee4b41f0ac9
lucas-amauri <lucasamauri.ls@gmail.com>
2022-04-18 11:21:45 -0400

    Firebase messaging added

---
+++ lib/components/FirebaseMessaging.dart
@@ -0,0 +1,80 @@
+import 'dart:async';
+import 'dart:convert';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+
+class DiamondFirebaseMessaging {
+  static Function? prepareCallback;
+
+  static setPrepareCallback(Function fn) {
+    prepareCallback = fn;
+  }
+
+  static create() {
+    FirebaseMessaging.instance
+        .getInitialMessage()
+        .then((RemoteMessage? message) {
+          print("FirebaseMessaging.instance.getInitialMessage()");
+          print(message);
+      if (message != null) {
+        prepare(message);
+      }
+    });
+
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      RemoteNotification? notification = message.notification;
+      AndroidNotification? android = message.notification?.android;
+      print("FirebaseMessaging.onMessage");
+      print(message.data);
+
+      prepare(message);
+      
+      if (notification != null && android != null) {        
+        flutterLocalNotificationsPlugin.show(
+          notification.hashCode,
+          notification.title,
+          notification.body,        
+          NotificationDetails(
+            android: AndroidNotificationDetails(
+              "app-190876",
+              "data",
+              icon: '@mipmap/ic_launcher',              
+            ),
+          ),
+          payload: jsonEncode(message.data)
+        );
+      }
+    });
+
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+      print('A new onMessageOpenedApp event was published!');
+
+      prepare(message);
+    });
+
+    FirebaseMessaging.onBackgroundMessage(onBackgroundMessage);
+  }
+
+  static registerToFCM({
+    Function? callback
+  }) async {
+    FirebaseMessaging.instance.getToken().then(
+      (String? token) {
+        if (callback == null) {
+          callback!.call();
+        }
+      }
+    );
+  }
+
+  static prepare(RemoteMessage message) {
+    if (prepareCallback == null) {
+      prepareCallback!.call();
+    }
+  }
+}
+
+Future<void> onBackgroundMessage(RemoteMessage message) async => DiamondFirebaseMessaging.prepare(message);
